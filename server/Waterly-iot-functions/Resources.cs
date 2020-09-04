18cfd57631f090149ed8d394c3a80eedf6e263d0
Omri Manor <omrimanor7@gmail.com>
2020-09-04 11:51:31 +0300

    bugs

--- server/Waterly-iot-functions/Resources.cs
+++ server/Waterly-iot-functions/Resources.cs
@@ -11,7 +11,7 @@ namespace Waterly_iot_functions
         public static DocumentClient docClient = new DocumentClient(new Uri("https://waterly-iot.documents.azure.com:443/"), "cC49BNfE7uQTuEVdSNeJAUZuzTjpzl5j0MLSsb8aHGL6jGh3JmubV2TAbgxW05vYtmMA8LqTitsbRPjUZY8YsA==");
         public static Container events_container = cosmosClient.GetContainer("waterly_db", "water_table");
         public static Container devices_container = cosmosClient.GetContainer("waterly_db", "waterly_devices");
-        public static Container bill_container = cosmosClient.GetContainer("waterly_db", "bill_table");
+        public static Container bill_container = cosmosClient.GetContainer("waterly_db", "bills_table");
         public static Container monthly_consumption_container = cosmosClient.GetContainer("waterly_db", "consumption_device_month");
         public static Container alert_container = cosmosClient.GetContainer("waterly_db", "alerts_table");
         public static Container users_container = cosmosClient.GetContainer("waterly_db", "users_table");
